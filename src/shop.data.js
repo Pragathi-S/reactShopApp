9d21e5fb5b3a706110c0f5ff0b736a5533b5bc64
Pragathi S <84302402+Pragathi-S@users.noreply.github.com>
2021-09-14 17:18:10 +0530

    Update shop.data.js

--- src/shop.data.js
+++ src/shop.data.js
@@ -15,7 +15,7 @@ let shopData = [
         item: [{product: "Sale Item",
         price1: "$50.00",
         price2: " $25.00"},{product: "Special Item",
-        price1: "$20.00",price2: "$18.00"},, {product: "Popular Item",
+        price1: "$20.00",price2: "$18.00"}, {product: "Popular Item",
         price1: "$40.00"}]
     }  
 ];
